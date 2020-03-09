b60d3986f445e591df99197f1ef0c9c85600f34e
tgpethan <ethan@pyra-studios.net>
2020-03-09 22:59:31 +0000

    Take extra code from rvhttp out

--- TestServer.js
+++ TestServer.js
@@ -1,6 +1,7 @@
 require('http').createServer((req, res) => handleRequest(req, res)).listen(7272, () => console.log("Dev server listening"));
 
 const fs = require('fs');
+
 function handleRequest(req, res)
 {
     const exec = require('child_process').exec;
@@ -8,37 +9,4 @@ function handleRequest(req, res)
         console.log(stdout)
         return res.end(fs.readFileSync("./compiled.css"));
     });
-    return;
-    let urlEnd = getLastItem("", req.url);
-    if (urlEnd === "/")
-    {
-        res.end("INDEX");
-    } else
-    {
-        urlEnd = getLastItem("/", req.url);
-        let fileExtention = getLastItem(".", urlEnd);
-        if (isImage(fileExtention)) {
-            res.end("Picture - " + fileExtention + " - " + req.url);
-        } else
-        {
-            res.end("NOT INDEX - " + fileExtention + " - " + req.url);
-        }
-        
-    }
-
-    return;
-}
-
-function getLastItem(s, toSplit)
-{
-    return toSplit.split(s).pop();
-}
-
-function isImage(s) {
-    if (s === "png" || s === "jpg" || s === "jpeg" || s === "gif") {
-        return true;
-    } else 
-    {
-        return false;
-    }
 }

7c7201d347002bc5a711edebab5e31f6bcbed3f5
Ethan Stubbs <ethan@pyra-studios.net>
2020-05-04 05:11:34 +0100

    Make TestServer.js actually sane

--- TestServer.js
+++ TestServer.js
@@ -1,11 +1,30 @@
+const fs = require('fs'),
+      exec = require('child_process').exec;
+
+// Create server instance
 require('http').createServer((req, res) => handleRequest(req, res)).listen(7272, () => console.log("Dev server listening"));
 
-const fs = require('fs');
+function handleRequest(req, res) {
+    switch (mURS(req.url)) {
+        case "/":
+            return fs.readFile("./testPage/index.html", (e, b) => { if (e) throw e; else res.end(b); });
 
-function handleRequest(req, res)
-{
-    require('child_process').exec("node build", function(error, stdout, stderr) {
-        console.log(stdout.split("\n")[stdout.split("\n").length-2]);
-        return res.end(fs.readFileSync("./compiled.css"));
-    });
+        case "/latest.css":
+            // Compile the most recent changes
+            exec("node build", function(error, stdout, stderr) {
+                // Get the time taken to compile / total files compiled
+                console.log(stdout.split("\n")[stdout.split("\n").length-2]);
+                // Read the most recent compiled css and send it to the client
+                fs.readFile("./compiled.css", (e, b) => { if (e) throw e; else res.end(b); });
+            });
+        break;
+
+        default:
+            return res.end("404 | Not good.");
+    }
+}
+
+// Make url nice for the switchy witchy statement
+function mURS(url) {
+    return url.split("?")[0];
 }
